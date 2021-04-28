92da8e477da35c820fa6746b580ae1c63042adf6
Gabriel T. Nardy <gtnardy@gmail.com>
2021-04-28 08:19:56 -0300

    Added new Enums

--- NanosWorldLuaLib.lua
+++ NanosWorldLuaLib.lua
@@ -11,18 +11,19 @@ CrosshairType = { None = 0, Regular = 1, Circle = 2, Crossbow = 3, Dot = 4, Holo
 DamageType = { Shot = 0, Explosion = 1, Punch = 2, Fall = 3, RunOver = 4, Unknown = 5 }
 DifferentialType = { LimitedSlip_4W = 0, LimitedSlip_FrontDrive = 1, LimitedSlip_RearDrive = 2, Open_4W = 3, Open_FrontDrive = 4, Open_RearDrive = 5 }
 FallingMode = { None = 0, Jumping = 1, Climbing = 2, Vaulting = 3, Falling = 4, HighFalling = 5, Parachuting = 6, SkyDiving = 7 }
-FontType= { Roboto = 0, GothicA1 = 1, PoiretOne = 2 }
+FontType = { Roboto = 0, GothicA1 = 1, PoiretOne = 2, Oswald = 3, RobotoMono = 4, OpenSans = 5 }
 GaitMode = { None = 0, Walking = 1, Sprinting = 2 }
 HandlingMode = { SingleHandedWeapon = 0, DoubleHandedWeapon = 1, SingleHandedMelee = 2, DoubleHandedMelee = 3, Throwable = 4, Torch = 5, Barrel = 6, Box = 7 }
 LightType = { Point = 0, Spot = 1, React = 2 }
 LogType = { Display = 0, Warning = 1,	Error = 2, Debug = 3, Verbose = 4, Scripting = 5, Chat = 6, WebUI = 7 }
-MaterialType = { Masked = 1, Translucent = 2 }
+MaterialType = { None = 0, Masked = 1, Translucent = 2, TranslucentDepth = 3 }
 SoundType = { SFX = 0, Music = 1 }
 StanceMode = { None = 0, Standing = 1, Crouching = 2, Proning = 3 }
 SurfaceType = { Default = 0, Carpet = 1, Concrete = 2, Grass = 3, Gravel = 4, Ground = 5, MetalLight = 6, Plastic = 7, Sand = 8, Snow = 9, Water = 10, WoodLight = 11, Flesh = 12, MetalHeavy = 13, WoodHeavy = 14, Ice = 15, Mud = 16, Rock = 17, Thump = 18, Glass = 19 }
 SwimmingMode = { None = 0, Superficie = 1, Underwater = 2 }
+TextRenderAlignCamera = { Unaligned = 0, AlignCameraRotation = 1, FaceCamera = 2 }
+TextRenderBevelType = { Linear = 0, HalfCircle = 1, Convex = 2, Concave = 3, OneStep = 4, TwoSteps = 5, Engraved = 6 }
 TextRenderHorizontalAlignment = { Left = 0, Center = 1, Right = 2 }
-TextRenderType = { Lit = 0, Unlit = 1, UnlitAlwaysVisible = 2 }
 TextRenderVerticalAlignment = { Top = 0, Center = 1, Bottom = 2, QuadTop = 3 }
 TriggerType = { Sphere = 0, Box = 1 }
 ViewMode = { FPS = 0, TPS1 = 1, TPS2 = 2, TPS3 = 3 }
