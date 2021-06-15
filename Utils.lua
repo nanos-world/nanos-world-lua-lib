8b195ab3e5c4f7302c5cd230be3e41c3bf1939dd
GTNardy <gtnardy@gmail.com>
2021-06-15 11:07:15 -0300

    Update Utils.lua

--- Utils.lua
+++ Utils.lua
@@ -3,7 +3,7 @@ print = function(...)
 	return Package:Log(...)
 end
 
-table.insert(package.searchers, function(modul_name)
+table.insert(package.searchers, function(module_name)
 	local success, result = pcall(Package.Require, Package, module_name)
 	if success then
 		return function() return result end
