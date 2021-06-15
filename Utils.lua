cb8828b6f2f00f458017e93cf3074d19ee64694f
Gabriel Francisco <gabrinelson27@gmail.com>
2021-06-15 10:55:47 -0300

    Remove dump function

--- Utils.lua
+++ Utils.lua
@@ -7,28 +7,3 @@ print = function(...)
 
     return Package:Log(toprint)
 end
-
-local function dumptable(t, indent, done)
-    done = done or {}
-    indent = indent or 0
-
-    done[t] = true
-
-    for key, value in pairs(t) do
-        print(string.rep("\t", indent))
-
-        if type(value) == "table" and not done[value] then
-            done[value] = true
-            print(key, ":\n")
-
-            dumptable(value, indent + 2, done)
-            done[value] = nil
-        else
-            print(key, "\t=\t", value, "\n")
-        end
-    end
-end
-
-function dump(t)
-    dumptable(t)
-end
