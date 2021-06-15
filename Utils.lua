fc53cc4bc3c74edd435085afcbbe44f470e477a3
Gabriel Francisco <gabrinelson27@gmail.com>
2021-06-15 11:07:43 -0300

    Add a package searcher to add files using require through Package.Require

--- Utils.lua
+++ Utils.lua
@@ -2,3 +2,10 @@
 print = function(...)
 	return Package:Log(...)
 end
+
+table.insert(package.searchers, function(module_name)
+	local success, result = pcall(Package.Require, Package, module_name)
+	if success then
+		return function() return result end
+	end
+end)
