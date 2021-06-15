700f7c06ed156d40297456ba5b504afb5b80e7af
Gabriel T. Nardy <gtnardy@gmail.com>
2021-06-15 11:13:24 -0300

    Add commentary to searchers

--- Utils.lua
+++ Utils.lua
@@ -8,6 +8,7 @@ print = function(...)
 	return Package:Log(toprint)
 end
 
+-- Adds 'require' to searchers
 table.insert(package.searchers, function(module_name)
 	local success, result = pcall(Package.Require, Package, module_name)
 	if success then
