fa1b30fb84f9b54eb5a038cd70cc61e1af6ee578
Gabriel Tanasovici Nardy <gtnardy@gmail.com>
2025-05-26 07:50:24 -0300

    Added default randomseed call to shared lib

--- Shared.lua
+++ Shared.lua
@@ -1,6 +1,6 @@
 -- Overrides print to call Console Log instead
 print = function(...)
-	-- Table used to store the final output, which will be concatted in the end
+	-- Table used to store the final output, which will be concatenated in the end
 	local buffer = {}
 
 	-- Cache table.insert as local because it's faster
@@ -11,6 +11,9 @@ print = function(...)
 		table_insert(buffer, "\t")
 	end
 
-	-- After all, concats the results
+	-- After all, concatenate the results
 	return Console.Log(table.concat(buffer))
 end
+
+-- Generates a random seed based on the current time for this session
+math.randomseed(os.time())
